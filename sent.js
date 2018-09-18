0d9df95a782d93f1f1e9988c4f7e5e53fc08e985
Noel V <noel.vashishtha@gmail.com>
2018-09-17 21:25:33 -0400

    first

---
+++ sent.js
@@ -0,0 +1,8 @@
+var sentL = [];
+sentL.push("life had started dealing him aces again");
+sentL.push("he told anecdotes about his job");
+sentL.push("a suit of armour");
+sentL.push("if you have any queries please telephone our office");
+sentL.push("she will look elegant in black");
+sentL.push("an uncanny feeling that she was being watched");
+

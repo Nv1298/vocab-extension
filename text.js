31d3b563dd1c6b669deb6ff36dbf8b048d85dafa
Noel V <noel.vashishtha@gmail.com>
2018-09-17 21:27:43 -0400

    Add files via upload

---
+++ text.js
@@ -0,0 +1,14 @@
+var defL = []; 
+defL.push('a playing card with a single spot on it, ranked as the highest card in its suit in most card games'); 
+defL.push('a short amusing or interesting story about a real incident or person'); 
+defL.push('a sudden feeling of cold with shivering accompanied by a rise in temperature, often with copious sweating, especially at the onset or height of a fever');
+defL.push('a question, especially one expressing doubt or requesting information');
+defL.push('graceful and stylish in appearance or manner');
+defL.push('strange or mysterious, especially in an unsettling way');
+
+
+
+
+
+
+
